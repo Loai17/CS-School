05b02c912696a799981313d29a0ab37b1e62da24
loai17 <loai.qubti@gmail.com>
2018-03-21 12:22:35 +0200

    started adding firebase to Login- not finished

--- androidloginsignupbasic-master/app/src/main/java/com/example/user/loginsignup/JobsManager.java
+++ androidloginsignupbasic-master/app/src/main/java/com/example/user/loginsignup/JobsManager.java
@@ -2,12 +2,31 @@ package com.example.user.loginsignup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Adapter;
 
 public class JobsManager extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private Database db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_jobs_manager);
+        setContentView(R.layout.activity_jobs_manager_main);
+
+//        initialize();
     }
+
+//    private void addItem(String item) {
+//        mItems.add(item);
+//        mAdapter.notifyDataSetChanged();
+////    }
+//
+//    private void initialize() {
+//        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+//        db = new Database(this);
+//
+//        recyclerView.set
+//    }
 }
